d27e9ab5ea14c568262c5fe82ca404cf7ca532d4
LGooding <LGooding@users.noreply.github.com>
2015-08-23 15:24:09 +1000

    Update token_ring.ads
    Changed names of data types in anticipation that they are going to be generic later. Updated to handle two seperate tokens as well I guess that might be worth mentioning...

--- token_ring.ads
+++ token_ring.ads
@@ -1,17 +1,20 @@
 --
--- Liam Gooding 2015
+-- Liam M Gooding, 2015
 --
 
 package Token_Ring is
 
+   type Status_Token_Type is new Character; -- leave these as chars for the time being, cahnge to generic later
+   type Data_Token_Type is new Character;
    type Node;
    type Task_Index is mod 8;
 
    -- a task to receive the token messages, dispatches contents of token to worker task to handle
    task type Node_Task is
       entry Initialise (In_Node : Node);
-      entry Send_Token (Token : Character); -- called by another task to send the token to this node
-      entry Receive_Token (Token : Character); -- called by the worker task to return the token to be sent 
+      entry Receive_Status (Status_Token : Status_Token_Type);
+      entry Send_Data (Data_Token : Data_Token_Type); -- called by another task to send the token to this node
+      entry Receive_Data_Token (Data_Token : Data_Token_Type); -- called by the worker task to return the token to be sent
    end Node_Task;
 
    type Node_Ptr is access all Node_Task;
@@ -19,7 +22,7 @@ package Token_Ring is
    -- a task to process the tokens away from the message handler
    task type Worker_Task is
       entry Set_Parent_Task (Parent_Task : Node_Ptr);
-      entry Handle_Token (Token : Character); -- called by Node_Task to send the token to this task for processing
+      entry Handle_Token (Token : Data_Token_Type); -- called by Node_Task to send the token to this task for processing
    end Worker_Task;
 
    type Worker_Ptr is access all Worker_Task;
