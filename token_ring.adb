ea3ccfe9daf9aa9c19b719a7167cb73583c4f6a3
LGooding <LGooding@users.noreply.github.com>
2015-08-23 15:25:27 +1000

    Update token_ring.adb
    Added capacity to handle both the status and data token.

--- token_ring.adb
+++ token_ring.adb
@@ -1,5 +1,5 @@
 --
--- Liam Gooding - u5011681
+-- Liam M Gooding, 2015
 --
 
 pragma Initialize_Scalars;
@@ -13,7 +13,8 @@ package body Token_Ring is
       Next_Node : Node_Ptr;
       Worker : Worker_Ptr; -- address to the linked task to process the token
       Node_Id : Task_Index;
-      Local_Token : Character;
+      Local_Data_Token : Data_Token_Type;
+      Local_Status_Token : Status_Token_Type;
 
    begin
       loop
@@ -24,25 +25,32 @@ package body Token_Ring is
                Worker := In_Node.Status_Worker;
             end Initialise;
          or
+            accept Receive_Status (Status_Token : Status_Token_Type) do
+               Local_Status_Token := Status_Token;
+               Put_Line ("Node" & Task_Index'Image (Node_Id) & " has received the STATUS token");
+               delay 0.1;
+            end Receive_Status;
+            Next_Node.all.Receive_Status (Local_Status_Token);
+         or
             -- being sent a token from the previous node
             -- pass it off to the worker task
-            accept Send_Token (Token : Character) do
-               Put_Line ("Node" & Task_Index'Image (Node_Id) & " has received Status Token");
-               Local_Token := Token;
-            end Send_Token;
-            Worker.all.Handle_Token (Local_Token);
+            accept Send_Data (Data_Token : Data_Token_Type) do
+               Put_Line ("Node" & Task_Index'Image (Node_Id) & " has received the DATA token");
+               Local_Data_Token := Data_Token;
+            end Send_Data;
+            Worker.all.Handle_Token (Local_Data_Token);
          or
-            -- The worker task is finished processing the token and returns it via this function
-            accept Receive_Token (Token : Character) do
-               Local_Token := Token;
-            end Receive_Token;
-            Next_Node.all.Send_Token (Token => Local_Token);
+            -- The worker task is finished processing the data token and returns it via this function
+            accept Receive_Data_Token (Data_Token : Data_Token_Type) do
+               Local_Data_Token := Data_Token;
+            end Receive_Data_Token;
+            Next_Node.all.Send_Data (Data_Token => Local_Data_Token);
          end select;
       end loop;
    end Node_Task;
 
    task body Worker_Task is
-      Local_Token : Character;
+      Local_Data_Token : Data_Token_Type;
       Parent_Node : Node_Ptr;
    begin
       loop
@@ -51,12 +59,12 @@ package body Token_Ring is
                Parent_Node := Parent_Task;
             end Set_Parent_Task;
          or
-            accept Handle_Token (Token : Character) do
-               Put_Line ("Sub-Task has received the token " & Character'Image (Token) & ". Processing and Sending to the master Node");
-               delay 1.0; -- simulate handling the token
-               Local_Token := Token;
+            accept Handle_Token (Token : Data_Token_Type) do
+               -- Put_Line ("Sub-Task has received the DATA token. Processing and returning to the parent node");
+               delay 0.5; -- simulate handling the token
+               Local_Data_Token := Token;
             end Handle_Token;
-            Parent_Node.all.Receive_Token (Local_Token); -- .all is the dereference
+            Parent_Node.all.Receive_Data_Token (Local_Data_Token); -- .all is the dereference
          end select;
       end loop;
    end Worker_Task;
