429261342f95c649f38c0cfb4b6a931889ea27d0
LGooding <LGooding@users.noreply.github.com>
2015-10-12 17:31:09 +1100

    Update generator_controllers.adb

--- generator_controllers.adb
+++ generator_controllers.adb
@@ -2,53 +2,57 @@
 -- Uwe R. Zimmer, Australia 2015
 --
 
-with Ada.Interrupts.Names;          use Ada.Interrupts.Names;
-with Ada.Real_Time;                 use Ada.Real_Time;
-with ANU_Base_Board;                use ANU_Base_Board;
-with ANU_Base_Board.Com_Interface;  use ANU_Base_Board.Com_Interface;
-with ANU_Base_Board.LED_Interface;  use ANU_Base_Board.LED_Interface;
-with Discovery_Board;               use Discovery_Board;
-with Discovery_Board.Button;        use Discovery_Board.Button;
-with Discovery_Board.LED_Interface; use Discovery_Board.LED_Interface;
-with STM32F4;                       use type STM32F4.Bit, STM32F4.Bits_32;
-with System;                        use System;
-
-with STM32F4.Random_number_generator.Ops; use STM32F4.Random_number_generator.Ops;
-with STM32F4.Reset_and_clock_control.Ops; use STM32F4.Reset_and_clock_control.Ops;
+with Ada.Interrupts.Names;                use Ada.Interrupts.Names;
+with Discovery_Board;                     use Discovery_Board;
+with Discovery_Board.LED_Interface;       use Discovery_Board.LED_Interface;
+with STM32F4;                             use type STM32F4.Bit, STM32F4.Bits_32;
 with STM32F4.Timers.Ops;                  use STM32F4.Timers.Ops;
+with System;                              use System;
 
 package body Generator_Controllers is
 
-   protected Timer_Underflow with Interrupt_Priority => Interrupt_Priority'First is
+   protected Timer_Update with Interrupt_Priority => Interrupt_Priority'First is
 
       function Current_State return Oscillator_State;
-
+      
    private
       procedure Interrupt_Handler with Attach_Handler => TIM2_Interrupt;
-      -- attempting to match this interrupt with the hardware set flag, probably wrong
       pragma Unreferenced (Interrupt_Handler);
 
       State     : Oscillator_State := Off;
-   end Timer_Underflow;
+   end Timer_Update;
 
-   protected body Timer_Underflow is
+   protected body Timer_Update is
 
       function Current_State return Oscillator_State is (State);
 
       procedure Interrupt_Handler is
 
       begin
-         Clear_Flag (No => 2, This_Flag => Update); -- clearing the interrupt so others can be triggered
-         State     := (if State = On then Off else On);
-
-         if State = On then
-            Discovery_Board.LED_Interface.All_On;
-         else
+         STM32F4.Timers.Ops.Clear_Flag (No => 2, This_Flag => Update);
+         if State = Off then
             Discovery_Board.LED_Interface.All_Off;
+         else
+            Discovery_Board.LED_Interface.All_On;
          end if;
+         -- toggle LEDs for time being, later output signal
          
       end Interrupt_Handler;
 
-   end Timer_Underflow;
+   end Timer_Update;
 
+   function Current_Oscillator_State return Oscillator_State is (Timer_Update.Current_State);
+
+   procedure Initialize is
+
+   begin
+      STM32F4.Timers.Ops.Enable (No => 2);
+      STM32F4.Timers.Ops.Set_Auto_Reload_32 (No => 2, Auto_Reload => 320_000);
+      STM32F4.Timers.Ops.Enable (No  => 2, Int => Update);
+      -- need to set the system to allow the interrupt as well, pin 28 of the vector table
+      
+   end Initialize;
+
+begin
+   Initialize;
 end Generator_Controllers;
