94a061eb501d16eb9a5d484492c5d79eec015ec5
LGooding <LGooding@users.noreply.github.com>
2015-10-13 14:14:57 +1100

    Update generator_controllers.adb
    The timer wasn't globally enabled with the RCC register, because yeah... Anyway it works now. Need to be careful with the on/off logic and the oscillation speed when attempting to verify that things are working.

--- generator_controllers.adb
+++ generator_controllers.adb
@@ -7,6 +7,7 @@ with Discovery_Board;                     use Discovery_Board;
 with Discovery_Board.LED_Interface;       use Discovery_Board.LED_Interface;
 with STM32F4;                             use type STM32F4.Bit, STM32F4.Bits_32;
 with STM32F4.Timers.Ops;                  use STM32F4.Timers.Ops;
+with STM32F4.Reset_and_clock_control.Ops; use STM32F4.Reset_and_clock_control.Ops;
 with System;                              use System;
 
 package body Generator_Controllers is
@@ -14,7 +15,7 @@ package body Generator_Controllers is
    protected Timer_Update with Interrupt_Priority => Interrupt_Priority'First is
 
       function Current_State return Oscillator_State;
-      
+
    private
       procedure Interrupt_Handler with Attach_Handler => TIM2_Interrupt;
       pragma Unreferenced (Interrupt_Handler);
@@ -29,14 +30,16 @@ package body Generator_Controllers is
       procedure Interrupt_Handler is
 
       begin
-         STM32F4.Timers.Ops.Clear_Flag (No => 2, This_Flag => Update);
-         if State = Off then
+         if State = On then
             Discovery_Board.LED_Interface.All_Off;
-         else
+            State := Off;
+         else 
             Discovery_Board.LED_Interface.All_On;
+            State := On;
          end if;
+         STM32F4.Timers.Ops.Clear_Flag (No => 2, This_Flag => Update);
          -- toggle LEDs for time being, later output signal
-         
+
       end Interrupt_Handler;
 
    end Timer_Update;
@@ -46,11 +49,13 @@ package body Generator_Controllers is
    procedure Initialize is
 
    begin
+      STM32F4.Reset_and_clock_control.Ops.Enable (No => 2);
       STM32F4.Timers.Ops.Enable (No => 2);
-      STM32F4.Timers.Ops.Set_Auto_Reload_32 (No => 2, Auto_Reload => 320_000);
+      STM32F4.Timers.Ops.Set_Auto_Reload_32 (No => 2, Auto_Reload => 16_000_000);
       STM32F4.Timers.Ops.Enable (No  => 2, Int => Update);
+
       -- need to set the system to allow the interrupt as well, pin 28 of the vector table
-      
+
    end Initialize;
 
 begin
