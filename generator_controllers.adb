74e90abd50a90eabf3ae2e079ddc916b8279530e
LGooding <LGooding@users.noreply.github.com>
2015-10-15 13:15:19 +1100

    Update generator_controllers.adb
    Need to be using a record or some other reference instead of the magic numbers for setting the interrupt sources with the COM Ports. Also still need to set up masking and the triggers. Also need to add sending capabilities and a falling edge interrupt.

--- generator_controllers.adb
+++ generator_controllers.adb
@@ -1,14 +1,18 @@
 --
--- Uwe R. Zimmer, Australia 2015
+-- Liam M. Gooding, Australia 2015
 --
 
-with Ada.Interrupts.Names;                use Ada.Interrupts.Names;
-with Discovery_Board;                     use Discovery_Board;
-with Discovery_Board.LED_Interface;       use Discovery_Board.LED_Interface;
-with STM32F4;                             use type STM32F4.Bit, STM32F4.Bits_32;
-with STM32F4.Timers.Ops;                  use STM32F4.Timers.Ops;
-with STM32F4.Reset_and_clock_control.Ops; use STM32F4.Reset_and_clock_control.Ops;
-with System;                              use System;
+with Ada.Interrupts.Names;                        use Ada.Interrupts.Names;
+with ANU_Base_Board.Com_Interface;                use ANU_Base_Board.Com_Interface;
+with Discovery_Board;                             use Discovery_Board;
+with Discovery_Board.LED_Interface;               use Discovery_Board.LED_Interface;
+with STM32F4;                                     use type STM32F4.Bit, STM32F4.Bits_32;
+with STM32F4.General_purpose_IOs;                 use STM32F4.General_purpose_IOs;
+with STM32F4.Timers.Ops;                          use STM32F4.Timers.Ops;
+with STM32F4.Reset_and_clock_control.Ops;         use STM32F4.Reset_and_clock_control.Ops;
+with STM32F4.Interrupts_and_Events.Ops;           use STM32F4.Interrupts_and_Events.Ops;
+with STM32F4.System_configuration_controller.Ops; use STM32F4.System_configuration_controller.Ops;
+with System;                                      use System;
 
 package body Generator_Controllers is
 
@@ -31,31 +35,75 @@ package body Generator_Controllers is
 
       begin
          if State = On then
-            Discovery_Board.LED_Interface.All_Off;
+            Discovery_Board.LED_Interface.On (LED => Red);
+            Discovery_Board.LED_Interface.Off (LED => Green);
+            -- stop writing to all output pins
             State := Off;
-         else 
-            Discovery_Board.LED_Interface.All_On;
+         else
+            Discovery_Board.LED_Interface.Off (LED => Red);
+            Discovery_Board.LED_Interface.On (LED => Green);
+            -- write to all output pins
             State := On;
          end if;
          STM32F4.Timers.Ops.Clear_Flag (No => 2, This_Flag => Update);
-         -- toggle LEDs for time being, later output signal
 
       end Interrupt_Handler;
 
    end Timer_Update;
+
+   protected Incoming_Rising_Edge with Interrupt_Priority => Interrupt_Priority'First is
+
+   private
+      procedure Interrupt_Handler;
+      -- two of the COM Ports use the same external interrupt line :/ will this be a problem?
+      pragma Attach_Handler (Interrupt_Handler, EXTI9_5_Interrupt);   -- COM Port 1
+      pragma Attach_Handler (Interrupt_Handler, EXTI9_5_Interrupt);   -- COM Port 2
+      pragma Attach_Handler (Interrupt_Handler, EXTI15_10_Interrupt); -- COM Port 3
+      pragma Attach_Handler (Interrupt_Handler, EXTI15_10_Interrupt); -- COM Port 3
+      
+      pragma Unreferenced (Interrupt_Handler);
+   end Incoming_Rising_Edge;
+
+   protected body Incoming_Rising_Edge is
+
+      procedure Interrupt_Handler is
+         -- flash com lights
+      begin 
+         ANU_Base_Board.LED_Interface.On (LED => 1); -- For COM port 1
+
+      end Interrupt_Handler;
+
+   end Incoming_Rising_Edge;
 
    function Current_Oscillator_State return Oscillator_State is (Timer_Update.Current_State);
 
    procedure Initialize is
 
    begin
+
+      -- Setting up the Oscillator
       STM32F4.Reset_and_clock_control.Ops.Enable (No => 2);
       STM32F4.Timers.Ops.Enable (No => 2);
-      STM32F4.Timers.Ops.Set_Auto_Reload_32 (No => 2, Auto_Reload => 16_000_000);
-      STM32F4.Timers.Ops.Enable (No  => 2, Int => Update);
+      STM32F4.Timers.Ops.Set_Auto_Reload_32 (No => 2, Auto_Reload => 16_000_000); -- counting up is the default, need to change this, hear that the manual reccomends low prescaler value and high clock division
+      STM32F4.Timers.Ops.Enable (No => 2, Int => Update); -- what interrupts are masked for this?
 
-      -- need to set the system to allow the interrupt as well, pin 28 of the vector table
+      -- Setting up the COM Ports to receive and send signals
 
+      -- Enabling the GPIO ports attached to the COM Ports
+      Enable (B);
+      Enable (C);
+      Enable (D);
+
+      Enable (System_Configuration_Contr); -- not sure what the function of this line is
+
+      Set_Interrupt_Source (Interrupt_No => 7,  Port => B);  -- COM Port 1, not sure if these are called correctly, look at the button example, also should change to reference not a magic number
+      Set_Interrupt_Source (Interrupt_No => 6,  Port => D);  -- COM Port 2
+      Set_Interrupt_Source (Interrupt_No => 11, Port => C);  -- COM Port 3
+      Set_Interrupt_Source (Interrupt_No => 2,  Port => D);  -- COM Port 4
+
+      Set_Trigger (Line => 7, Raising => Enable, Falling => Disable);
+      Masking (Line => Pin, State => Unmasked);
+      
    end Initialize;
 
 begin
